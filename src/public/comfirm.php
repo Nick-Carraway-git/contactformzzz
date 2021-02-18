b95eabe20443dcb72a50541d2f6a457abe860077
Yuma Tsukumo <yuma@iMac.local>
2021-02-18 23:28:00 +0900

    create php templete

---
+++ src/public/comfirm.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <title>Contact Form Comfirm</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="format.css">
+  </head>
+  <body>
+
+  </body>
+</html>
